379758d4f4689b397bba16a60759b6fdd59fd880
jorgejbas <jorgejbas@fab.mil.br>
2019-09-18 15:29:38 -0300

    alteracao do config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 function abrirConexao() {
 	
-	$con = @mysql_connect("10.80.16.3", "root","123456");
+	$con = @mysql_connect("10.80.16.3", "root","manutencao");
 	
 	if (! $con) {
 		die ("Erro ao abrir a conexao com o MySQL: " . mysql_error ());
@@ -19,6 +19,6 @@ function abrirConexao() {
     $sigla_om = 'SEREP-RF';
     $site_om = 'www.SEREPRF.intraer';
     $igla_setor = 'SEREP-RF'; // Sigla da seção responsável por receber os relatórios
-    $email_setor = 'brunabsp@ala15.intraer'; //endereço de e-mail para onde o sistema envia os relatórios
+//    $email_setor = 'brunabsp@ala15.intraer'; //endereço de e-mail para onde o sistema envia os relatórios
     $telefone_setor = '(81)3461-7483';
 ?>
